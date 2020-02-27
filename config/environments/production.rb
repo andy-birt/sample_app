9c3ec81b51ac4856ad5ee47eb9888dd9666eccef
Andrew Birt <abirt1986@gmail.com>
2020-02-27 08:17:59 -0500

    modify production settings for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: "587",
     authentication: :plain,
-    username: env["SENDGRID_USERNAME"],
-    password: env["SENDGRID_PASSWORD"],
+    username: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
     enable_starttls_auto: true
   }
